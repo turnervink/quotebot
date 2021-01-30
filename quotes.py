fe2e994c456a6ce67ad19a1e7d36a051f1bde4af
Turner Vink <turner.vink@gmail.com>
2021-01-30 09:56:11 -0800

    Added a custom help command

--- quotes.py
+++ quotes.py
@@ -31,7 +31,16 @@ class Quotes(commands.Cog):
         await ctx.send(f"{ctx.message.author.mention} Quote added!")
         await ctx.message.delete()
 
+    @commands.command(name="help")
+    async def help(self, ctx):
+        embed = discord.Embed(title="Quote Bot Help")
+        embed.add_field(name="Get a quote:", value="`$quote`", inline=False)
+        embed.add_field(name="Add a new quote:",
+                        value="`$addquote \"Your cool quote\" \"Author Name\" \"January 1st, 1970\"`", inline=False)
+        await ctx.send(embed=embed)
+
 
 def setup(bot: commands.Bot):
+    bot.remove_command("help")
     bot.add_cog(Quotes(bot))
 
