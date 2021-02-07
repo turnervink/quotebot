bad1889274ec69730dddebed91be89a2187f1b0b
Turner Vink <turner.vink@gmail.com>
2021-02-06 16:16:35 -0800

    Get message when quote add reaction is added

--- bot.py
+++ bot.py
@@ -1,9 +1,20 @@
+import discord
 from discord.ext import commands
 
 import os
 
 
 bot = commands.Bot(command_prefix="$")
+
+
+@bot.event
+async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
+    if str(payload.emoji) == "📇":
+        channel = await bot.fetch_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        print(message.content)
+    else:
+        print("Not match")
 
 
 @bot.event
